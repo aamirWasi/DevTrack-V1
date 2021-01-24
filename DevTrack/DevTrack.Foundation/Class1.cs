321bdff22dbcc7e7508a6ac02c274fba979ecfc5
Asnoyon <blackpain1100@gmail.com>
2021-01-24 20:21:57 +0600

    Initial Setup Done

--- DevTrack/DevTrack.Foundation/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DevTrack.Foundation
-{
-    public class Class1
-    {
-    }
-}
---
+++ DevTrack/DevTrack.Foundation/FoundationModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+
+namespace DevTrack.Foundation
+{
+    public class FoundationModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+
+            base.Load(builder);
+        }
+    }
+}
