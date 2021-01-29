c4fd58e92302d4672927b9f0e0e1a48244cd908f
Tawkirhq <tawkirhq@gmail.com>
2021-01-29 22:10:41 +0600

    tracker class added

---
+++ DevTrack/DevTrack.Foundation/Services/TrackerService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevTrack.Foundation.Services
+{
+    public class TrackerService : ITrackerService
+    {
+        private readonly ISnapShotService _snapShotService;
+
+        public TrackerService(ISnapShotService snapShotService)
+        {
+            _snapShotService = snapShotService;
+        }
+
+        public void Track()
+        {
+            _snapShotService.SnapshotCapturer();
+        }
+    }
+}
