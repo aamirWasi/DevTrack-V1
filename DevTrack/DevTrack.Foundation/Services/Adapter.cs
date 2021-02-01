a79430345a0af559705ef957ea176486947c465c
aamirWasi <wasi3aamir@gmail.com>
2021-02-01 17:53:28 +0600

    modified snapshot service & using bitmap class with the help of adapter pattern & remove a concreate dependencies from snapshotservice class

---
+++ DevTrack/DevTrack.Foundation/Services/Adapter.cs
@@ -0,0 +1,25 @@
+﻿using System.Drawing;
+
+namespace DevTrack.Foundation.Services
+{
+    public class Adapter : IAdapter
+    {
+        public int Width { get; set; }
+        public string FilePath { get; set; }
+        public int Height { get; set; }
+        public Image Image { get; set; }
+
+        private Image _bitMap;
+
+        public Adapter(int width, int height)
+        {
+            _bitMap = new Bitmap(width, height);
+            Image = _bitMap;
+        }
+
+        public void SaveImage(string filePath)
+        {
+            _bitMap.Save(filePath);
+        }
+    }
+}
--- DevTrack/DevTrack.Foundation/Services/SnapShotService.cs
+++ DevTrack/DevTrack.Foundation/Services/SnapShotService.cs
@@ -9,73 +9,33 @@ namespace DevTrack.Foundation.Services
 {
     public class SnapShotService : ISnapShotService
     {
-        public int Width { get; set; }
-        public int Height { get; set; }
         public string FilePath { get; set; }
-        public string AlternativeText { get; set; }
+        public DateTimeOffset CaptureTime { get; set; }
 
-        private readonly ISnapshotUnitOfWork _snapshotUnitOfWork;
+        private ISnapshotUnitOfWork _snapshotUnitOfWork;
+        private IBitMapAdapter _image;
 
-        public SnapShotService(ISnapshotUnitOfWork snapshotUnitOfWork)
+        public SnapShotService(ISnapshotUnitOfWork snapshotUnitOfWork,IBitMapAdapter image)
         {
             _snapshotUnitOfWork = snapshotUnitOfWork;
+            _image = image;
         }
-
+     
         public void SnapshotCapturer()
         {
-            var snapshot = GenerateSnapshot();
+            var snapshot = _image.GenerateSnapshot2();
             var image = snapshot.image;
             if (image == null)
                 throw new InvalidOperationException("Image information is missing");
 
             var imageEntity = new EO.SnapshotImage
             {
+                CaptureTime = DateTimeOffset.Now,
                 FilePath = snapshot.fileLoaction
             };
 
             _snapshotUnitOfWork.SnapshotRepository.Add(imageEntity);
             _snapshotUnitOfWork.Save();
-        }
-
-        private (Image image, string fileLoaction) GenerateSnapshot()
-        {
-            Bitmap _image;
-            float dpiX, dpiY;
-
-            using (Graphics graphics = Graphics.FromHwnd(IntPtr.Zero))
-            {
-                dpiX = graphics.DpiX * 20;
-                dpiY = graphics.DpiY * 11.25f;
-            }
-
-            Width = (int)Math.Round(dpiX);
-            Height = (int)Math.Round(dpiY);
-
-            _image = new Bitmap(Width, Height, PixelFormat.Format32bppArgb);
-
-            var s = new Size(_image.Width, _image.Height);
-
-            var imageGraphics = Graphics.FromImage(_image);
-            imageGraphics.CopyFromScreen(0, 0, 0, 0, s);
-            string newPath = GetDirectoryPath();
-            bool exists = Directory.Exists(newPath);
-            string imgName = DateTime.Now.ToString("(dd_MMMM_hh_mm_ss_tt)");
-            if (!exists)
-            {
-                Directory.CreateDirectory(newPath);
-            }
-            FilePath = newPath +
-                      @"\Snapshot" + "_" + imgName
-                      + ".jpeg";
-            _image.Save(FilePath);
-            return (_image, FilePath);
-        }
-
-        private static string GetDirectoryPath()
-        {
-            var path = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
-            var imageDirectory = "ScreenShotReceiver";
-            return Path.Combine(path, imageDirectory);
         }
     }
 }
