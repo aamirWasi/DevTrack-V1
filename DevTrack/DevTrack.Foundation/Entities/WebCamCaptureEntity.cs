fed7e4a8a3c7f68416dbab881f68f9c6556bcbb6
Ashif Ahmed Khan <mirajashif@gmail.com>
2021-02-02 00:38:36 +0600

    Added entities

---
+++ DevTrack/DevTrack.Foundation/Entities/WebCamCaptureEntity.cs
@@ -0,0 +1,16 @@
+﻿using DevTrack.DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevTrack.Foundation.Entities
+{
+    class WebCamCaptureEntity : IEntity<int>
+    {
+        public int Id { get; set; }
+
+        public string WebCamImagePath { get; set; }
+
+        public DateTime WebCamImageDateTime { get; set; }
+    }
+}
