e95ba60d114d84c64dcda3183eb994692a92d4ab
aamirWasi <wasi3aamir@gmail.com>
2021-02-01 23:25:55 +0600

    added another test case to check image is saved

--- DevTrack/DevTrack.Foundation.Tests/Services/SnapShotServiceTests.cs
+++ DevTrack/DevTrack.Foundation.Tests/Services/SnapShotServiceTests.cs
@@ -1,4 +1,5 @@
 ﻿using Autofac.Extras.Moq;
+using DevTrack.Foundation.Entities;
 using DevTrack.Foundation.Repositories;
 using DevTrack.Foundation.Services;
 using DevTrack.Foundation.UnitOfWorks;
@@ -69,6 +70,32 @@ namespace DevTrack.Foundation.Tests.Services
             this.ShouldSatisfyAllConditions(
                 () => _bitMapAdapterMock.VerifyAll()
                 ,() => _adpaterMock.VerifyAll()
+                );
+        }
+
+        [Test]
+        public void SnapshotCapturer_ImageProvided_SaveImage()
+        {
+            //arrange
+            IAdapter image = new Adapter(1000, 1000);
+            const string filePath = @"D:/test";
+            var imageEntity = new SnapshotImage() { CaptureTime = DateTimeOffset.Now, FilePath = filePath };
+            (IAdapter image, string filePath) obj = (image, filePath);
+
+            _snapshotUnitOfWorkMock.Setup(x => x.SnapshotRepository).Returns(_snapshotRepositoryMock.Object);
+            _bitMapAdapterMock.Setup(x => x.GenerateSnapshot()).Returns(obj);
+            _snapshotRepositoryMock.Setup(x => x.Add(It.Is<SnapshotImage>(y => y.FilePath == imageEntity.FilePath))).Verifiable();
+            _snapshotUnitOfWorkMock.Setup(x => x.Save()).Verifiable();
+
+            //act
+            _snapshotService.SnapshotCapturer();
+
+            //assert
+            this.ShouldSatisfyAllConditions(
+                () => _bitMapAdapterMock.VerifyAll()
+                ,() => _adpaterMock.VerifyAll()
+                , () => _snapshotUnitOfWorkMock.VerifyAll()
+                , () => _snapshotRepositoryMock.VerifyAll()
                 );
         }
     }
