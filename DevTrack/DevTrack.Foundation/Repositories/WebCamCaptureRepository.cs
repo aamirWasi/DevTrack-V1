bf87f238dd250d756cea8fadcafa688a53c4c634
Ashif Ahmed Khan <mirajashif@gmail.com>
2021-02-03 01:02:45 +0600

    created unitofwork

--- DevTrack/DevTrack.Foundation/Repositories/WebCamCaptureRepository.cs
+++ DevTrack/DevTrack.Foundation/Repositories/WebCamCaptureRepository.cs
@@ -4,9 +4,9 @@ using DevTrack.Foundation.Entities;
 
 namespace DevTrack.Foundation.Repositories
 {
-    public class WebCamCaptureRepository : Repository<SnapshotImage, int, DevTrackContext>
+    public class WebCamCaptureRepository : Repository<WebCamCaptureEntity, int, DevTrackContext>, IWebCamCaptureRepository
     {
-        public WebCamCaptureRepository(DevTrackContext WebCamContext) : base(WebCamContext)
+        public WebCamCaptureRepository(DevTrackContext devTrackContext) : base(devTrackContext)
         {
 
         }
---
+++ DevTrack/DevTrack.Foundation/UnitOfWorks/WebCamCaptureUnitOfWork.cs
@@ -0,0 +1,21 @@
+﻿using DevTrack.DataAccessLayer;
+using DevTrack.Foundation.Contexts;
+using DevTrack.Foundation.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevTrack.Foundation.UnitOfWorks
+{
+    public class WebCamCaptureUnitOfWork : UnitOfWork, IWebCamCaptureUnitOfWork
+    {
+        public IWebCamCaptureRepository webCamCaptureRepository { get; set; }
+
+        public WebCamCaptureUnitOfWork(DevTrackContext webCamContext, IWebCamCaptureRepository webCamRepository) : base(webCamContext)
+        {
+            webCamCaptureRepository = webCamRepository;
+        }
+    }
+}
