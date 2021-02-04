4e50576c6c3a3d3d7fa1497e38efeb182da33181
ismail5g <ismail27.dec@gmail.com>
2021-02-04 22:58:49 +0600

    Keyboard Entity Migrated Successfully

--- DevTrack/DevTrack.Foundation/Contexts/DevTrackContext.cs
+++ DevTrack/DevTrack.Foundation/Contexts/DevTrackContext.cs
@@ -10,6 +10,7 @@ namespace DevTrack.Foundation.Contexts
         public DbSet<ActiveProgram> ActivePrograms { get; set; }
         public DbSet<RunningProgram> RunningPrograms { get; set; }
         public DbSet<WebCamCaptureImage> WebCamCapture { get; set; }
+        public DbSet<Keyboard> Keyboards { get; set; }
 
         private readonly string _connectionString;
         private readonly string _migrationAssemblyName;
