b6abe84790dbbe34c71e928f562b84651dfed91f
Ashif Ahmed Khan <mirajashif@gmail.com>
2021-02-09 23:28:17 +0600

    Test Setup complete

--- DevTrack/DevTrack.Foundation.Tests/Services/WebCamCaptureTests.cs
+++ DevTrack/DevTrack.Foundation.Tests/Services/WebCamCaptureTests.cs
@@ -3,13 +3,22 @@ using System.Collections.Generic;
 using System.Text;
 using Autofac.Extras.Moq;
 using NUnit.Framework;
+using DevTrack.Foundation.UnitOfWorks;
+using DevTrack.Foundation.Repositories;
+using DevTrack.Foundation.Adapters;
 using Shouldly;
+using Moq;
+using DevTrack.Foundation.Services;
 
 namespace DevTrack.Foundation.Tests.Services
 {
     class WebCamCaptureTests
     {
         private AutoMock _mock;
+        private Mock<IWebCamCaptureUnitOfWork> _webCamCaptureUnitOfWorkMock;
+        private Mock<IWebCamCaptureRepository> _webCamCaptureRepositoryMock;
+        private Mock<IWebCamImageAdapter> _webCamImageAdapterMock;
+        private WebCamCaptureService _webCamCaptureService;
 
         [OneTimeSetUp]
         public void ClassTestUp()
@@ -26,7 +35,10 @@ namespace DevTrack.Foundation.Tests.Services
         [SetUp]
         public void Setup()
         {
-
+            _webCamCaptureRepositoryMock = _mock.Mock<IWebCamCaptureRepository>();
+            _webCamCaptureUnitOfWorkMock = _mock.Mock<IWebCamCaptureUnitOfWork>();
+            _webCamImageAdapterMock = _mock.Mock<IWebCamImageAdapter>();
+            _webCamCaptureService = _mock.Create<WebCamCaptureService>();
         }
 
         [TearDown]
