ae03d1097b973eb845afa3ece3710b0d901f83dc
Tawkirhq <tawkirhq@gmail.com>
2021-02-13 11:01:47 +0600

    seeding method updated

--- DevTrack/DevTrack.Web/Data/20210213045338_UserRolesSeed.cs
+++ DevTrack/DevTrack.Web/Data/20210213045338_UserRolesSeed.cs
@@ -3,24 +3,24 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace DevTrack.Web.Data
 {
-    public partial class SeedRoles : Migration
+    public partial class UserRolesSeed : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.InsertData(
                 table: "AspNetRoles",
                 columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
-                values: new object[] { new Guid("647bd95b-115c-4b29-8078-e97919a5d530"), "b6150c18-0979-4ba1-92f7-f984db661661", "Admin", "ADMIN" });
+                values: new object[] { new Guid("36c7d5a3-04e8-4c2f-9eb4-254f27a71b0c"), "a3e0ade2-3d2a-4068-b589-6477062ed604", "Admin", "ADMIN" });
 
             migrationBuilder.InsertData(
                 table: "AspNetRoles",
                 columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
-                values: new object[] { new Guid("93f86480-abc4-486d-9a4a-16c68e15d8dd"), "110df3f9-203f-4dd6-bbc7-e1a639978646", "Trainer", "TRAINER" });
+                values: new object[] { new Guid("0c8dd35c-c1b8-46e1-9ac7-1a5639d99ba0"), "f084d5a9-e09f-41bb-8308-ec5d63881392", "Trainer", "TRAINER" });
 
             migrationBuilder.InsertData(
                 table: "AspNetRoles",
                 columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
-                values: new object[] { new Guid("e5ee7021-4a67-405f-8bfb-bf6bc996cf5b"), "8f089daf-1e3a-4e95-b507-97dd38390f36", "Student", "STUDENT" });
+                values: new object[] { new Guid("a66e7ff3-9ddf-4fe7-b360-e763a3bb7a16"), "c7696fc8-b8fc-47c0-ad3d-3e083694fc63", "Student", "STUDENT" });
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
@@ -28,17 +28,17 @@ namespace DevTrack.Web.Data
             migrationBuilder.DeleteData(
                 table: "AspNetRoles",
                 keyColumn: "Id",
-                keyValue: new Guid("647bd95b-115c-4b29-8078-e97919a5d530"));
+                keyValue: new Guid("0c8dd35c-c1b8-46e1-9ac7-1a5639d99ba0"));
 
             migrationBuilder.DeleteData(
                 table: "AspNetRoles",
                 keyColumn: "Id",
-                keyValue: new Guid("93f86480-abc4-486d-9a4a-16c68e15d8dd"));
+                keyValue: new Guid("36c7d5a3-04e8-4c2f-9eb4-254f27a71b0c"));
 
             migrationBuilder.DeleteData(
                 table: "AspNetRoles",
                 keyColumn: "Id",
-                keyValue: new Guid("e5ee7021-4a67-405f-8bfb-bf6bc996cf5b"));
+                keyValue: new Guid("a66e7ff3-9ddf-4fe7-b360-e763a3bb7a16"));
         }
     }
 }
