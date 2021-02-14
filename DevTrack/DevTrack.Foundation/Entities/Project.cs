a22850a23b3ae35e61830f1466a1b4da465a2eb2
ismail5g <ismail27.dec@gmail.com>
2021-02-14 10:21:38 +0600

    Project & Settings Entity implemented

---
+++ DevTrack/DevTrack.Foundation/Entities/Project.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using DevTrack.DataAccessLayer;
+using DevTrack.Membership.Entities;
+
+namespace DevTrack.Foundation.Entities
+{
+    public class Project : IEntity<int>
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+        public ApplicationUser User { get; set; }
+        public bool IsAdmin { get; set; }
+        public Settings Settings { get; set; }
+    }
+}
