78495ed3c96c1033e5730c655f0568b522446159
Ashif Ahmed Khan <mirajashif@gmail.com>
2021-02-19 18:57:56 +0600

    created WebCamCaptureModel

---
+++ DevTrack/DevTrack.API/Models/WebCamCaptureModel.cs
@@ -0,0 +1,35 @@
+﻿using Autofac;
+using DevTrack.Foundation.Entities;
+using DevTrack.Foundation.Services;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace DevTrack.API.Models
+{
+    public class WebCamCaptureModel
+    {
+        public IFormFile FilePath { get; set; }
+        public DateTimeOffset CaptureTime { get; set; }
+
+        private ISnapShotWebService _snapShotWebService;
+
+        public WebCamCaptureModel()
+        {
+            _snapShotWebService = Startup.AutofacContainer.Resolve<ISnapShotWebService>();
+        }
+
+        public void SaveWebCamCapture()
+        {
+            if (FilePath != null)
+            {
+                var (fileName, filePath) = StoreFile(FilePath);
+
+                _snapShotWebService.SaveSnapShotWebDb(new SnapshotImage
+                {
+                    FilePath = filePath,
+                    CaptureTime = CaptureTime
+                });
+            }
+        }
+    }
+}
