1d2bee5fbac340ba22c856ea020f09af0101ab7f
Ashif Ahmed Khan <mirajashif@gmail.com>
2021-02-21 22:53:59 +0600

    added a business object

---
+++ DevTrack/DevTrack.Foundation/BusinessObjects/WebCamCapture.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace DevTrack.Foundation.BusinessObjects
+{
+    public class WebCamCapture
+    {
+        public string Location { get; set; }
+        public IFormFile FilePath { get; set; }
+    }
+}
