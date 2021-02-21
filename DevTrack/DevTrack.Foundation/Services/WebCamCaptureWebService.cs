7b4c22eb71b84ce173d521d0ba0722c1997e1670
Ashif Ahmed Khan <mirajashif@gmail.com>
2021-02-21 22:25:01 +0600

    added WebCamCaptureWebService

---
+++ DevTrack/DevTrack.Foundation/Services/WebCamCaptureWebService.cs
@@ -0,0 +1,29 @@
+﻿using DevTrack.Foundation.UnitOfWorks;
+using System;
+using DevTrack.Foundation.Entities;
+
+namespace DevTrack.Foundation.Services
+{
+    public class WebCamCaptureWebService
+    {
+        private readonly IWebCamCaptureUnitOfWork _webCamCaptureUnitOfWork;
+
+        public WebCamCaptureWebService(IWebCamCaptureUnitOfWork webCamCaptureUnitOfWork)
+        {
+            _webCamCaptureUnitOfWork = webCamCaptureUnitOfWork;
+        }
+
+        public void SaveSnapShotWebDb(WebCamCaptureImage image)
+        {
+            if (image == null)
+            {
+                throw new InvalidOperationException("Image information is  missing");
+            }
+            else
+            {
+                _webCamCaptureUnitOfWork._webCamCaptureRepository.Add(image);
+                _webCamCaptureUnitOfWork.Save();
+            }
+        }
+    }
+}
