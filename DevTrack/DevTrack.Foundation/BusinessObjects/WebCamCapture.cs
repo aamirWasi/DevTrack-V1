135d9b54a335765de2888fa53f19b3baae22bd48
Ashif Ahmed Khan <mirajashif@gmail.com>
2021-02-25 02:57:50 +0600

    updated business object

--- DevTrack/DevTrack.Foundation/BusinessObjects/WebCamCapture.cs
+++ DevTrack/DevTrack.Foundation/BusinessObjects/WebCamCapture.cs
@@ -1,10 +1,15 @@
 ﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DevTrack.Foundation.BusinessObjects
 {
     public class WebCamCapture
     {
-        public string Location { get; set; }
-        public IFormFile FilePath { get; set; }
+        public DateTimeOffset CapturerTime { get; set; }
+        public IFormFile Image { get; set; }
     }
 }
