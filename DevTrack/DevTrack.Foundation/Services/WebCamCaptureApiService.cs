8f6677ceaac18eadfaeb990e9b34227d9a794af0
Ashif Ahmed Khan <mirajashif@gmail.com>
2021-02-25 02:25:00 +0600

    added & inplemented WebCamCaptureApiService

---
+++ DevTrack/DevTrack.Foundation/Services/WebCamCaptureApiService.cs
@@ -0,0 +1,35 @@
+﻿using DevTrack.Foundation.Entities;
+using System;
+using System.IO;
+using System.Net.Http;
+
+namespace DevTrack.Foundation.Services
+{
+    public class WebCamCaptureApiService
+    {
+        public string SaveCampuredImageInSql(WebCamCaptureImage imageEntity)
+        {
+            var finalResult = string.Empty;
+            using (var httpClient = new HttpClient())
+            {
+                httpClient.BaseAddress = new Uri("https://localhost:44332/");
+
+                var file_bytes = File.ReadAllBytes(imageEntity.WebCamImagePath);
+
+                var form = new MultipartFormDataContent();
+                form.Add(new StringContent(imageEntity.WebCamImageDateTime.ToString("yyyy-MM-dd h:mm tt")), "CaptureTime");
+                form.Add(new ByteArrayContent(file_bytes, 0, file_bytes.Length), "FilePath", "file.jpeg");
+                using (var response = httpClient.PostAsync("api/Snapshot", form).Result)
+                {
+                    if (response.IsSuccessStatusCode)
+                    {
+                        using var content = response.Content;
+                        var result = content.ReadAsStringAsync();
+                        finalResult = result.Result;
+                    }
+                }
+            }
+            return finalResult;
+        }
+    }
+}
