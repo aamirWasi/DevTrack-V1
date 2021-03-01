91a82ba5fbadb04dc0c74b9d2fd02ddbd7209929
Ashif Ahmed Khan <mirajashif@gmail.com>
2021-03-01 22:05:55 +0600

    created unite of work

---
+++ DevTrack/DevTrack.Foundation/UnitOfWorks/IProjectUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using DevTrack.DataAccessLayer;
+using DevTrack.Foundation.Repositories;
+
+namespace DevTrack.Foundation.UnitOfWorks
+{
+    public interface IProjectUnitOfWork : IUnitOfWork
+    {
+        IProjectRepository projectRepository { get; set; }
+    }
+}
---
+++ DevTrack/DevTrack.Foundation/UnitOfWorks/ProjectUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using DevTrack.DataAccessLayer;
+using DevTrack.Foundation.Contexts;
+using DevTrack.Foundation.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevTrack.Foundation.UnitOfWorks
+{
+    public class ProjectUnitOfWork : UnitOfWork, IProjectUnitOfWork
+    {
+        public IProjectRepository projectRepository { get; set; }
+
+        public ProjectUnitOfWork(ProjectContext projectContext, IProjectRepository _projectRepository) : base(projectContext)
+        {
+            projectRepository = _projectRepository;
+        }
+    }
+}
