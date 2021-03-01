0bb9a20022861b61a52ba83e2b04af8941f56f2c
Ashif Ahmed Khan <mirajashif@gmail.com>
2021-03-01 22:06:33 +0600

    created project controller

---
+++ DevTrack/DevTrack.Web/Areas/Admin/Controllers/ProjectController.cs
@@ -0,0 +1,86 @@
+﻿using Autofac;
+using DevTrack.Web.Areas.Admin.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevTrack.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProjectController : Controller
+    {
+        // GET: ProjectController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ProjectController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ProjectController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ProjectController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            var NewModel = Startup.AutofacContainer.Resolve<ProjectCreateModel>();
+            
+            return View(NewModel);
+            
+        }
+
+        // GET: ProjectController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ProjectController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ProjectController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ProjectController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
