eda297132e05ba69155a57aaed410afbcc157391
Ashif Ahmed Khan <mirajashif@gmail.com>
2021-03-01 22:06:51 +0600

    created project context

---
+++ DevTrack/DevTrack.Foundation/Contexts/IProjectContext.cs
@@ -0,0 +1,10 @@
+﻿using DevTrack.Foundation.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevTrack.Foundation.Contexts
+{
+    public interface IProjectContext
+    {
+        DbSet<Project> Project { get; set; }
+    }
+}
---
+++ DevTrack/DevTrack.Foundation/Contexts/ProjectContext.cs
@@ -0,0 +1,41 @@
+﻿using DevTrack.Foundation.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DevTrack.Foundation.Contexts
+{
+    public class ProjectContext : DbContext, IProjectContext
+    {
+        public DbSet<Project> Project { get; set; }
+
+        private readonly string _connectionString;
+        private readonly string _migrationAssemblyName;
+
+        public ProjectContext(string connectionString, string migrationAssemblyName)
+        {
+            _connectionString = connectionString;
+            _migrationAssemblyName = migrationAssemblyName;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder dbContextOptionsBuilder)
+        {
+            if (!dbContextOptionsBuilder.IsConfigured)
+            {
+                dbContextOptionsBuilder.UseSqlServer(
+                    _connectionString,
+                    m => m.MigrationsAssembly(_migrationAssemblyName));
+            }
+
+            base.OnConfiguring(dbContextOptionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
