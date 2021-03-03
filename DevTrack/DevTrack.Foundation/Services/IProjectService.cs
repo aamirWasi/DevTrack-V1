ee7fe2b6f350e7f92c9c14acd6431d6b59ab5c58
Ashif Ahmed Khan <mirajashif@gmail.com>
2021-03-03 18:04:59 +0600

    created project service

---
+++ DevTrack/DevTrack.Foundation/Services/IProjectService.cs
@@ -0,0 +1,12 @@
+﻿using DevTrack.Foundation.BusinessObjects;
+
+namespace DevTrack.Foundation.Services
+{
+    public interface IProjectService
+    {
+        void CreateProject(Project project);
+        void DeleteProject(int id);
+        void EditProject(Project project);
+        void GetAllProject();
+    }
+}
---
+++ DevTrack/DevTrack.Foundation/Services/ProjectService.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EO = DevTrack.Foundation.Entities;
+using BO = DevTrack.Foundation.BusinessObjects;
+using DevTrack.Foundation.UnitOfWorks;
+
+namespace DevTrack.Foundation.Services
+{
+    public class ProjectService : IProjectService
+    {
+        private readonly ProjectUnitOfWork _projectUnitOfWork;
+
+        public ProjectService()
+        { }
+
+        public ProjectService(ProjectUnitOfWork projectUnitOfWork)
+        {
+            _projectUnitOfWork = projectUnitOfWork;
+        }
+
+        public void CreateProject(BO.Project project)
+        {
+            var projectEntity = new EO.Project
+            {
+                Name = project.Name,
+                CreateDate = project.CreationTime,
+                IsAdmin = project.IsAdmin
+            };
+
+            projectEntity.Settings = new EO.Settings
+            {
+                AllowTracking = project.Settings.AllowTracking,
+                TrackActiveProgram = project.Settings.TrackActiveProgram,
+                TakeScreenShot = project.Settings.TakeScreenShot,
+                TrackKeyboardHits = project.Settings.TrackKeyboardHits,
+                TrackMouseHits = project.Settings.TrackMouseHits,
+                TrackRunningProgram = project.Settings.TrackRunningProgram,
+                WebCamCapture = project.Settings.WebCamCapture
+            };
+
+            _projectUnitOfWork.projectRepository.Add(projectEntity);
+            //_projectUnitOfWork.Save();
+        }
+
+        public void EditProject(BO.Project project)
+        {
+
+        }
+
+        public void DeleteProject(int id)
+        {
+
+        }
+
+        public void GetAllProject()
+        {
+
+        }
+    }
+}
