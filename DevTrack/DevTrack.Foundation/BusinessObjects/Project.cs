457adcbe66fe0f3040b60716a024f3b8fe8113d6
Ashif Ahmed Khan <mirajashif@gmail.com>
2021-03-03 18:04:17 +0600

    created project Business Object

---
+++ DevTrack/DevTrack.Foundation/BusinessObjects/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using EO = DevTrack.Foundation.Entities;
+using BO = DevTrack.Foundation.BusinessObjects;
+
+namespace DevTrack.Foundation.BusinessObjects
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        //public ApplicationUser User { get; set; }
+        public bool IsAdmin { get; set; }
+        public DateTime CreationTime;
+        public BO.Settings Settings { get; set; }
+    }
+}
