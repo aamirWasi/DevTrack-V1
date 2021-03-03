1c7556b24ff272a43a856a0da315db04b6b31b4b
Ashif Ahmed Khan <mirajashif@gmail.com>
2021-03-03 18:05:53 +0600

    updated project model

--- DevTrack/DevTrack.Web/Areas/Admin/Models/ProjectCreateModel.cs
+++ DevTrack/DevTrack.Web/Areas/Admin/Models/ProjectCreateModel.cs
@@ -1,12 +1,13 @@
 ﻿using DevTrack.Foundation.BusinessObjects;
-using DevTrack.Foundation.Entities;
-using DevTrack.Foundation.UnitOfWorks;
+using BO = DevTrack.Foundation.BusinessObjects;
 using DevTrack.Membership.Entities;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using DevTrack.Foundation.Services;
+using Autofac;
 
 namespace DevTrack.Web.Areas.Admin.Models
 {
@@ -17,28 +18,64 @@ namespace DevTrack.Web.Areas.Admin.Models
         public string Name { get; set; }
         //public ApplicationUser User { get; set; }
         public bool IsAdmin { get; set; }
-        public Settings Settings { get; set; }
+        public DateTime CreationTime = DateTime.Now;
+        public BO.Settings Settings { get; set; }
 
-        private readonly IProjectUnitOfWork _projectUnitOfWork;
+        private readonly IProjectService _projectService;
 
         public ProjectCreateModel()
-        { }
-
-        public ProjectCreateModel(IProjectUnitOfWork projectUnitOfWork)
         {
-            _projectUnitOfWork = projectUnitOfWork;
+            _projectService = Startup.AutofacContainer.Resolve<IProjectService>();
         }
 
         public void CreateProject()
         {
-            var ProjectEntity = new Project
+            var project = ConvertToProjectBO();
+            _projectService.CreateProject(project);
+        }
+
+
+        //public void CreateProject()
+        //{
+        //    var ProjectEntity = new EO.Project
+        //    {
+        //        Name = Name,
+        //        CreateDate = DateTime.Now,
+        //        IsAdmin = IsAdmin                
+        //    };
+
+        //    ProjectEntity.Settings = new EO.Settings
+        //    {
+        //        AllowTracking = Settings.AllowTracking,
+        //        TrackActiveProgram = Settings.TrackActiveProgram,
+        //        TakeScreenShot = Settings.TakeScreenShot,
+        //        TrackKeyboardHits = Settings.TrackKeyboardHits,
+        //        TrackMouseHits = Settings.TrackMouseHits,
+        //        TrackRunningProgram = Settings.TrackRunningProgram,
+        //        WebCamCapture = Settings.WebCamCapture
+        //    };
+
+        //    //ProjectEntity.User = new ApplicationUser
+        //    //{
+        //    //    FullName = User.FullName,
+        //    //    ImageUrl = User.ImageUrl
+        //    //};
+
+
+        //    //_projectUnitOfWork.projectRepository.Add(ProjectEntity);
+        //    //_projectUnitOfWork.Save();
+        //}
+
+        private BO.Project ConvertToProjectBO()
+        {
+            var ProductBO = new BO.Project
             {
                 Name = Name,
-                CreateDate = DateTime.Now,
-                IsAdmin = IsAdmin                
+                CreationTime = CreationTime,
+                IsAdmin = IsAdmin
             };
 
-            ProjectEntity.Settings = new Settings
+            ProductBO.Settings = new Settings
             {
                 AllowTracking = Settings.AllowTracking,
                 TrackActiveProgram = Settings.TrackActiveProgram,
@@ -49,15 +86,8 @@ namespace DevTrack.Web.Areas.Admin.Models
                 WebCamCapture = Settings.WebCamCapture
             };
 
-            //ProjectEntity.User = new ApplicationUser
-            //{
-            //    FullName = User.FullName,
-            //    ImageUrl = User.ImageUrl
-            //};
-
-
-            _projectUnitOfWork.projectRepository.Add(ProjectEntity);
-            _projectUnitOfWork.Save();
+            return ProductBO;
         }
+
     }
 }
