75d5e704995ac6995a9c84bdac242d387018a0cd
Ashif Ahmed Khan <mirajashif@gmail.com>
2021-03-03 23:57:42 +0600

    Migration successfull

---
+++ DevTrack/DevTrack.Web/Migrations/20210303134151_DevTrackProject.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DevTrack.Web.Migrations
+{
+    public partial class DevTrackProject : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Settings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AllowTracking = table.Column<bool>(nullable: false),
+                    TakeScreenShot = table.Column<bool>(nullable: false),
+                    WebCamCapture = table.Column<bool>(nullable: false),
+                    TrackKeyboardHits = table.Column<bool>(nullable: false),
+                    TrackMouseHits = table.Column<bool>(nullable: false),
+                    TrackRunningProgram = table.Column<bool>(nullable: false),
+                    TrackActiveProgram = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Settings", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Project",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    CreateDate = table.Column<DateTime>(nullable: false),
+                    IsAdmin = table.Column<bool>(nullable: false),
+                    SettingsId = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Project", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Project_Settings_SettingsId",
+                        column: x => x.SettingsId,
+                        principalTable: "Settings",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Project_SettingsId",
+                table: "Project",
+                column: "SettingsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Project");
+
+            migrationBuilder.DropTable(
+                name: "Settings");
+        }
+    }
+}
