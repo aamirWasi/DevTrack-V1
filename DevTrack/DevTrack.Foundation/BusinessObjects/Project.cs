a1d888d34b669bd46852a03a733fb442357bda71
Ashif Ahmed Khan <mirajashif@gmail.com>
2021-03-03 23:58:00 +0600

    project BO updated

--- DevTrack/DevTrack.Foundation/BusinessObjects/Project.cs
+++ DevTrack/DevTrack.Foundation/BusinessObjects/Project.cs
@@ -14,5 +14,48 @@ namespace DevTrack.Foundation.BusinessObjects
         public bool IsAdmin { get; set; }
         public DateTime CreationTime;
         public BO.Settings Settings { get; set; }
+        static IList<BO.Project> ProjectList { get; set; }
+
+        public static Project ConvertToSelf(EO.Project project)
+        {
+            var projectBO = new Project
+            {
+                Name = project.Name,
+                IsAdmin = project.IsAdmin,
+                CreationTime = project.CreateDate
+            };
+
+            projectBO.Settings = new Settings
+            {
+                Id = project.Settings.Id,
+                AllowTracking = project.Settings.AllowTracking,
+                TrackActiveProgram = project.Settings.TrackActiveProgram,
+                TakeScreenShot = project.Settings.TakeScreenShot,
+                TrackKeyboardHits = project.Settings.TrackKeyboardHits,
+                TrackMouseHits = project.Settings.TrackMouseHits,
+                TrackRunningProgram = project.Settings.TrackRunningProgram,
+                WebCamCapture = project.Settings.WebCamCapture
+            };
+
+            return projectBO;
+        }
+
+        public static IList<Project> ConvertToProjectList(IList<EO.Project> projectList1)
+        {
+            ProjectList = new List<Project>();
+
+            foreach (var project in projectList1)
+            {
+                var obj = new BO.Project
+                {
+                    Id = project.Id,
+                    Name = project.Name
+                };
+
+                ProjectList.Add(obj);
+            }
+
+            return ProjectList;
+        }
     }
 }
