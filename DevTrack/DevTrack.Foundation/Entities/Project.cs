e6d5aedc95e7a02119a4d668b4995617881656ee
Ashif Ahmed Khan <mirajashif@gmail.com>
2021-03-05 23:53:42 +0600

    ApplicationUser removed from Project entity

--- DevTrack/DevTrack.Foundation/Entities/Project.cs
+++ DevTrack/DevTrack.Foundation/Entities/Project.cs
@@ -14,8 +14,7 @@ namespace DevTrack.Foundation.Entities
         public Guid UserId { get; set; }
         public string Name { get; set; }
         public DateTime CreateDate { get; set; }
-        [NotMapped]
-        public ApplicationUser User { get; set; }
+        //public ApplicationUser User { get; set; }
         public bool IsAdmin { get; set; }
         public Settings Settings { get; set; }
         
