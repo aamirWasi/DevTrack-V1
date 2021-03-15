6429d31c5dd697aa310b8b5f919cfb72d06add77
aamirWasi <wasi3aamir@gmail.com>
2021-03-15 13:36:34 +0600

    added webadapter service  to resolve httprequest

--- DevTrack/DevTrack.Foundation/Services/SnapShotWebService.cs
+++ DevTrack/DevTrack.Foundation/Services/SnapShotWebService.cs
@@ -1,18 +1,19 @@
 ﻿using DevTrack.Foundation.UnitOfWorks;
 using System;
 using DevTrack.Foundation.Entities;
-using System.Net.Http;
-using System.IO;
+using DevTrack.Foundation.Adapters;
 
 namespace DevTrack.Foundation.Services
 {
     public class SnapShotWebService : ISnapShotWebService
     {
         private readonly ISnapshotWebUnitOfWork _snapshotWebUnitOfWork;
+        private readonly ISnapShotWebAdapterService _snapShotWebAdapterService;
 
-        public SnapShotWebService(ISnapshotWebUnitOfWork snapshotWebUnitOfWork)
+        public SnapShotWebService(ISnapshotWebUnitOfWork snapshotWebUnitOfWork, ISnapShotWebAdapterService snapShotWebAdapterService)
         {
             _snapshotWebUnitOfWork = snapshotWebUnitOfWork;
+            _snapShotWebAdapterService = snapShotWebAdapterService;
         }
 
         public void SaveSnapShotWebDb(SnapshotImage image)
@@ -30,27 +31,10 @@ namespace DevTrack.Foundation.Services
 
         public string SaveSnapshotInSql(SnapshotImage imageEntity)
         {
-            var finalResult = string.Empty;
-            using (var httpClient = new HttpClient())
-            {
-                httpClient.BaseAddress = new Uri("https://localhost:44332/");
-
-                var file_bytes = File.ReadAllBytes(imageEntity.FilePath);
-
-                var form = new MultipartFormDataContent();
-                form.Add(new StringContent(imageEntity.CaptureTime.ToString("yyyy-MM-dd h:mm tt")), "CaptureTime");
-                form.Add(new ByteArrayContent(file_bytes, 0, file_bytes.Length), "FilePath", "file.jpeg");
-                using (var response = httpClient.PostAsync("api/Snapshot", form).Result)
-                {
-                    if (response.IsSuccessStatusCode)
-                    {
-                        using var content = response.Content;
-                        var result = content.ReadAsStringAsync();
-                        finalResult = result.Result;
-                    }
-                }
-            }
-            return finalResult;
+            if (imageEntity != null)
+                return _snapShotWebAdapterService.WebHttpResponse(imageEntity);
+            else
+                throw new InvalidOperationException("Image information is not valid");
         }
     }
 }
